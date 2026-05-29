73783238e7251df10ab059fa2eb3eae3bb7df3bb
ryuta114729 <ryuuta.tatemoto@van-nw.jp>
2026-05-29 15:08:48 +0900

    add static root

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ set -o errexit
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
-python3 manage.py superuser
--- recipeproject/settings.py
+++ recipeproject/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
